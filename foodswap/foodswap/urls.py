d52036015d1c209a0d2502ae096f4570d5b1cc57
Fabien Lenoir <flenoir@gmail.com>
2019-06-04 23:10:26 +0200

    added first route

--- foodswap/foodswap/urls.py
+++ foodswap/foodswap/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
+    # path(route, view, kwargs, name)
     path('admin/', admin.site.urls),
+    # path('foodsearch/', include('foodsearch.urls')),
+    path('', include('foodsearch.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+# from django.conf import settings
+# from django.conf.urls import include, url  # For django versions before 2.0
+# from django.urls import include, path  # For django versions from 2.0 and up
+
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [
+#         path('__debug__/', include(debug_toolbar.urls)),
+
+#         # For django versions before 2.0:
+#         # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+#     ] + urlpatterns
