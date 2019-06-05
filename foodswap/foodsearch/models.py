5f753836b801ae619bc48647003c21ef720390d5
Fabien Lenoir <flenoir@gmail.com>
2019-06-05 17:53:10 +0200

    added app in settings.py and created first Class in models.py

--- foodswap/foodsearch/models.py
+++ foodswap/foodsearch/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    # We specify the model fields
+    product_name = models.CharField(max_length=100)
+    brands = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=200, null=True)
+    product_url = models.CharField(max_length=150, null=True)
+    product_code = models.CharField(max_length=20, null=True)
+    product_image = models.CharField(max_length=100, null=True)
+    nutriscore = models.CharField(max_length=1, null=True)
+    stores = models.CharField(max_length=150, null=True)
+    quantity = models.CharField(max_length=40, null=True)
+    
